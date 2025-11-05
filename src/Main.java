4b1aea8f81816d193d7d0cde21dd97d8497bc0dc
soph <sophia@javamate.net>
2025-11-05 16:50:47 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> myArrList = new ArrayList<>();
+        myArrList.add("MILK");
+    }
+}
