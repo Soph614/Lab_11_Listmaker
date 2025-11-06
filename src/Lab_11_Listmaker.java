65d2e048161f170666f66be0e825ac74f40f2d8b
soph <sophia@javamate.net>
2025-11-06 15:40:21 -0500

    09/06/2025 3:40

--- src/Lab_11_Listmaker.java
+++ src/Lab_11_Listmaker.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -5,12 +6,23 @@ public class Lab_11_Listmaker {
     public static void main(String[] args) {
         Scanner pipe = new Scanner(System.in);
         ArrayList<String> myArrList = new ArrayList<>();
-        boolean done;
+        boolean yesOrNo;
+        boolean surelyDone = false;
+        boolean surelyNotDone = true;
         do {
-            String menuChoice = SafeInput.getRegExString(pipe, "Choose what you would like to do:\nA – Add an item to the list\nD – Delete an item from the list\nI – Insert an item into the list\nP – Print (i.e. display) the list\nQ – Quit the program\n   ", "[AaDdIiPpQq]");
-            System.out.println(menuChoice);
-            done = SafeInput.getYNConfirm(pipe, "If you would like to be done, press Y, if you want to continue, press N");
-        }while(!done);
+            String menuChoice = SafeInput.getRegExString(pipe, "A – Add an item to the list\nD – Delete an item from the list\nI – Insert an item into the list\nP – Print (i.e. display) the list\nQ – Quit the program\nChoose what you would like to do:", "[AaDdIiPpQq]");
+            if(menuChoice.equalsIgnoreCase("Q")) {
+                System.out.println("Goodbye!");
+                System.exit(0);
+            }
+            yesOrNo = SafeInput.getYNConfirm(pipe, "If you would like to be done, press Y, if you want to continue, press N");
+            if(yesOrNo) {
+                surelyDone = SafeInput.getYNConfirm(pipe, "Are you sure? ");
+            }
+            if(!yesOrNo) {
+                surelyNotDone = SafeInput.getYNConfirm(pipe, "Are you sure? ");
+            }
+        }while(surelyNotDone && !surelyDone);
 
     }
 }
